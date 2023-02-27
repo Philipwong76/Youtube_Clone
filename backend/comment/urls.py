c6ba408c20a339282bd28b91ad2dab68d5c27516
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-02-26 23:06:49 -0500

    Created comment/urls.py + views.py

---
+++ backend/comment/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.comment_list),
+    path('<int:pk>', views.comment_list),
+]
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def comment_list(request):
+
+    
+    return Response('ok')
