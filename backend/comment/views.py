db5cd51bc41425f647f4cae68177760d5fb51f6b
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-02-26 23:14:58 -0500

    modified views.py

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import status
+from .models import Comment
+from .serializers import CommentSerializer
+
 
 # Create your views here.
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def comment_list(request):
-
-    
-    return Response('ok')
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
