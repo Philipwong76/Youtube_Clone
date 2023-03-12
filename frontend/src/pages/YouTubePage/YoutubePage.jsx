f8f0799512d831203864f3f20089476faadcf159
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-12 12:24:13 -0400

    Created Youtubepage + VideoPage + SearchResultsPage as a starter

---
+++ frontend/src/pages/YouTubePage/YoutubePage.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const YouTubePage = () => {
+    return ( 
+        
+     );
+}
+ 
+export default YouTubePage;
